a949493ff9e8001027cde31f58548bc8c55b4b53
tommaso-moro <tomms1998@gmail.com>
2021-07-08 10:57:08 +0100

    First Commit

--- back-end/data/data_main.py
+++ back-end/data/data_main.py
@@ -1,10 +1,10 @@
 import mongo_client, full_archive_search_v2, batching, geo_data_cacher, batches_data_explorer
-from constants import BEARER_TOKEN, MONGO_DB_USERNAME, MONGO_DB_PASSWORD, MONGO_DB_NAME, MONGO_DB_ADDRESS
+from constants import BEARER_TOKEN, MONGO_DB_USERNAME, MONGO_DB_PASSWORD, MONGO_DB_NAME, MONGO_DB_ADDRESS, PROD_MONGO_COL_NAME
 
 
 def main():
     mongoClient = mongo_client.MongoClient(BEARER_TOKEN, MONGO_DB_USERNAME, MONGO_DB_PASSWORD, MONGO_DB_NAME, MONGO_DB_ADDRESS)
-
+    prodMongoCol = mongoClient.getMongoCollection(PROD_MONGO_COL_NAME)
 
 
 if __name__ == "__main__":
